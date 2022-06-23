fb73e4aff49aaf2951e9c6e8ed559183cf7c7c9d
BobrikovAleksey <bobrikov.spb@ya.ru>
2022-06-23 13:32:49 +0300

    Tic Tac Toe: Add game rules

--- react_tic-tac-toe/src/components/Board/index.js
+++ react_tic-tac-toe/src/components/Board/index.js
@@ -2,9 +2,37 @@ import React from 'react';
 
 import { Square } from './Square';
 
+/**
+ * Определяет победителя
+ * @param {string|null[]} squares
+ * @returns {string|boolean}
+ */
+const findWinner = (squares) => {
+  const lines = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+  for (let i = 0; i < lines.length; i += 1) {
+    const [ a, b, c ] = lines[i];
+    const A = squares[a], B = squares[b], C = squares[c];
+    if (A && B === A && C === A) return A;
+  }
+  return false;
+};
+
 class Board extends React.Component {
   state = {
     history: [ new Array(9).fill(null) ],
+  }
+
+  get gameOver() {
+    return (this.state.history.length > 9) || Boolean(findWinner(this.squares));
   }
 
   get nextPlayer() {
@@ -16,8 +44,14 @@ class Board extends React.Component {
     return history[history.length - 1];
   }
 
+  get status() {
+    if (!this.gameOver) return `Next player: ${this.nextPlayer}`;
+    if (findWinner(this.squares)) return `Winner ${findWinner(this.squares)}`;
+    return 'Game over';
+  }
+
   handleSquaresClick = (i) => {
-    if (this.squares[i]) return;
+    if (this.squares[i] || this.gameOver) return;
     const history = this.state.history.slice();
     const squares = this.squares.slice();
     history.push(squares);
@@ -33,11 +67,9 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.nextPlayer}`;
-
     return (
       <div>
-        <div className="status">{ status }</div>
+        <div className="status">{ this.status }</div>
         <div className="board-row">
           { this.renderSquare(1) }
           { this.renderSquare(2) }
